9c3efaa53c7133b4002e14afa6a8b9498a876a9b
Luke Kortunov <luke@kortunov.me>
2024-09-01 11:46:17 +0300

    Minor refactoring - Remove action parser compiler pass to execute logic in the runtime

--- src/DeadMansSwitchOpenApiSymfonyBundle.php
+++ src/DeadMansSwitchOpenApiSymfonyBundle.php
@@ -4,16 +4,6 @@ declare(strict_types=1);
 
 namespace DeadMansSwitch\OpenApi\Symfony;
 
-use DeadMansSwitch\OpenApi\Symfony\DependencyInjection\ActionParserCompilerPass;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-final class DeadMansSwitchOpenApiSymfonyBundle extends Bundle
-{
-    public function build(ContainerBuilder $container): void
-    {
-        parent::build($container);
-
-        $container->addCompilerPass(new ActionParserCompilerPass());
-    }
-}
+final class DeadMansSwitchOpenApiSymfonyBundle extends Bundle {}
